d4b1048a1f6e09e8674eb2ebf52a139bc588ea48
Advay Mengle <advay.mengle.dev1@gmail.com>
2015-06-02 13:37:24 -0700

    Skeleton for empty Gradle plugin build, issue #55 - Empty Groovy class for plugin. - Standard Gradle build wrapper
    For now we use com.github.brunobowden.j2objcgradle as our root package (i.e. group).

---
+++ src/main/groovy/com/github/brunobowden/j2objcgradle/J2objcPlugin.groovy
@@ -0,0 +1,31 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (c) 2015 the authors of j2objc-gradle (see AUTHORS file)
+ */
+
+package com.github.brunobowden.j2objcgradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+
+/**
+ * TODO: Document plugin once moved from j2objc.gradle.
+ */
+class J2objcPlugin implements Plugin<Project> {
+    @Override
+    def void apply(Project project) {
+        // TODO: Move j2objc.gradle functionality here.
+    }
+}
